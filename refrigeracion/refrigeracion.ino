bda888928ececb92c978a722044db996283aeedc
Gus Leyva <guleyva@uv.mx>
2022-08-03 18:16:29 -0500

    Create refrigeracion.ino

---
+++ refrigeracion/refrigeracion.ino
@@ -0,0 +1,39 @@
+//Programa de refrigeracion
+//Mayo 2022
+//El gus
+/*
+ensor de temperatura y humedad
+Boton1: Manual. Activa la refrigeración al ser presionado
+Boton2: Alta demanda
+Boton3: Sobre carga de funcionamiento
+
+Led1: Refrigeracion manual
+Led2: Refrigeración automática
+
+1. Que se encienda la refrigeracion manual cada que se presione el boton de manual
+
+2. La refrigeracion manual tiene prioridad a la refrigeracion automática.
+
+3. Si la termperatura es alta (mayor a 28, por ejemplo), se activa la refrigeracion automática
+
+4. Si tengo alta demanda o sobre carga, se activa la refrigeración automática
+
+5. Si tengo alta demanda o sobrecarga de funcionamiento y ademas temperatura alta, se activan ambas refrigeraciones
+
+Botones 14, 15, 13
+Leds 4, 2
+DHT11 12
+*/
+
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
